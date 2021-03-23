58e9b8d23c6954193670a74df384d75b6a6b2400
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2021-03-22 18:40:27 -0600

    Generate the ints randomly and improve exponent generation process. Use of seed to control randomness of p,q.

--- rsa.go
+++ rsa.go
@@ -1,17 +1,40 @@
 package rsa
 
+import (
+	"math"
+	"math/rand"
+)
+
 // GenerateKeys generate the public and private keys for RSA-16 bit
-// The output contains, N, exponent and private key in sequence
-func GenerateKeys(p int, q int) (int, int, int) {
-	// Very random, believe me
-	// p, q := 34667, 44207
+// The function takes a seed as input and generate Public Key N, exponent e, Private key pk
+func GenerateKeys(seed int) (int, int, int) {
+	p, q := generateRandomPrimes(seed)
 	N := p * q
 	phiN := (p - 1) * (q - 1)
 
-	// Very bad practice, but whatever
-	e := phiN - 1
+	e := generateExponent(phiN, 1<<16)
 	pk := modInverse(e, phiN)
 	return N, e, pk
+}
+
+func generateExponent(phiN int, max int) int {
+	for gcd(phiN, max) != 1 {
+		max--
+	}
+	return max
+}
+
+func gcd(a int, b int) int {
+	if a < b {
+		a, b = b, a
+	}
+	for b > 1 {
+		a, b = b, a%b
+	}
+	if b == 1 {
+		return 1
+	}
+	return a
 }
 
 // modInverse takes to values (num and mod in sequence) and calculate the modulo inverse of num with mod
@@ -32,7 +55,30 @@ func recurseModInverse(num1 int, num2 int, p0 int, p1 int) int {
 	}
 
 	p0, p1 = p1, (p0 - p1*(num1/num2))
-	print(num1, num1/num2, num1%num2, p1)
 	num1, num2 = num2, num1%num2
 	return recurseModInverse(num1, num2, p0, p1)
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func generateRandomPrimes(seed int) (int, int) {
+	rand.Seed(int64(seed))
+	min, max := 1<<15, 1<<16
+	p := rand.Intn(max-min) + min
+	for !isPrime(p) {
+		p = rand.Intn(max-min) + min
+	}
+	q := rand.Intn(max-min) + min
+	for !isPrime(q) {
+		q = rand.Intn(max-min) + min
+	}
+
+	return p, q
 }
